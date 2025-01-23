933a37bb8484b0eebd811fdfbf986d3e3cf823ef
sunsonsu <komson281@gmail.com>
2025-01-23 22:32:27 +0700

    clean version with contraction words

--- clean_data.py
+++ clean_data.py
@@ -2,7 +2,7 @@ import re
 import sys
 from nltk.corpus import stopwords
 import unicodedata
-
+import contractions
 def remove_non_words_from_file(input_file, output_file):
     # Function to clean the content of the file
     def clean_text(text):
@@ -10,6 +10,7 @@ def remove_non_words_from_file(input_file, output_file):
         text = re.sub(r'[^\w\s]', '', text)  # Remove non-word characters (excluding spaces)
         text = re.sub(r'[\U0001F600-\U0001F64F\U0001F300-\U0001F5FF\U0001F680-\U0001F6FF\U0001F700-\U0001F77F\U0001F780-\U0001F7FF\U0001F800-\U0001F8FF\U0001F900-\U0001F9FF\U0001FA00-\U0001FA6F\U0001FA70-\U0001FAFF\U00002702-\U000027B0]', '', text)  # Remove emojis
         #remove stop words
+        text = contractions.fix(text)
         stop_words = set(stopwords.words('english'))
         words = text.split()
         
