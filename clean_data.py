8a23f1db3c8a6ca483beff9fe317d8fa4785ff48
sunsonsu <komson281@gmail.com>
2025-01-26 12:34:27 +0700

    review-human  version

--- clean_data.py
+++ clean_data.py
@@ -1,22 +1,17 @@
 import re
 import sys
-from nltk.corpus import stopwords
 import unicodedata
 import contractions
+
 def remove_non_words_from_file(input_file, output_file):
     # Function to clean the content of the file
     def clean_text(text):
         # Remove non-alphanumeric characters (including special symbols like &#^!) and emojis
-        text = re.sub(r'[^\w\s]', '', text)  # Remove non-word characters (excluding spaces)
+        text = re.sub(r'[^\w\s\.\!\?\,\;\'\"]', '', text)  # modify to Keep punctuation marks
         text = re.sub(r'[\U0001F600-\U0001F64F\U0001F300-\U0001F5FF\U0001F680-\U0001F6FF\U0001F700-\U0001F77F\U0001F780-\U0001F7FF\U0001F800-\U0001F8FF\U0001F900-\U0001F9FF\U0001FA00-\U0001FA6F\U0001FA70-\U0001FAFF\U00002702-\U000027B0]', '', text)  # Remove emojis
-        #remove stop words
         text = contractions.fix(text)
-        stop_words = set(stopwords.words('english'))
-        words = text.split()
-        
-        return ' '.join([word for word in words if word not in stop_words])
 
-
+        return text
 
     # Read the content from the input file
     with open(input_file, 'r', encoding='utf-8') as file:
--- process.sh
+++ process.sh
@@ -31,36 +31,36 @@
 
 #clean human review
     # mkdir cleaned_review
-    # python3 clean_data.py use_this_review/review1.txt cleaned_review/cleaned_review1.txt
-    # python3 clean_data.py use_this_review/review2.txt cleaned_review/cleaned_review2.txt
-    # python3 clean_data.py use_this_review/review3.txt cleaned_review/cleaned_review3.txt
-    # python3 clean_data.py use_this_review/review5.txt cleaned_review/cleaned_review5.txt
-    # python3 clean_data.py use_this_review/review8.txt cleaned_review/cleaned_review8.txt
-    # python3 clean_data.py use_this_review/review10.txt cleaned_review/cleaned_review10.txt
+    python3 clean_data.py use_this_review/review1.txt cleaned_review/cleaned_review1.txt
+    python3 clean_data.py use_this_review/review2.txt cleaned_review/cleaned_review2.txt
+    python3 clean_data.py use_this_review/review3.txt cleaned_review/cleaned_review3.txt
+    python3 clean_data.py use_this_review/review5.txt cleaned_review/cleaned_review5.txt
+    python3 clean_data.py use_this_review/review8.txt cleaned_review/cleaned_review8.txt
+    python3 clean_data.py use_this_review/review10.txt cleaned_review/cleaned_review10.txt
 #clean ai review
     # mkdir cleaned_ai_review
-    # python3 clean_data.py use_this_ai_review/review1-ai.txt cleaned_ai_review/cleaned_review1-ai.txt
-    # python3 clean_data.py use_this_ai_review/review2-ai.txt cleaned_ai_review/cleaned_review2-ai.txt
-    # python3 clean_data.py use_this_ai_review/review3-ai.txt cleaned_ai_review/cleaned_review3-ai.txt
-    # python3 clean_data.py use_this_ai_review/review5-ai.txt cleaned_ai_review/cleaned_review5-ai.txt
-    # python3 clean_data.py use_this_ai_review/review8-ai.txt cleaned_ai_review/cleaned_review8-ai.txt
-    # python3 clean_data.py use_this_ai_review/review10-ai.txt cleaned_ai_review/cleaned_review10-ai.txt
+    python3 clean_data.py use_this_ai_review/review1-ai.txt cleaned_ai_review/cleaned_review1-ai.txt
+    python3 clean_data.py use_this_ai_review/review2-ai.txt cleaned_ai_review/cleaned_review2-ai.txt
+    python3 clean_data.py use_this_ai_review/review3-ai.txt cleaned_ai_review/cleaned_review3-ai.txt
+    python3 clean_data.py use_this_ai_review/review5-ai.txt cleaned_ai_review/cleaned_review5-ai.txt
+    python3 clean_data.py use_this_ai_review/review8-ai.txt cleaned_ai_review/cleaned_review8-ai.txt
+    python3 clean_data.py use_this_ai_review/review10-ai.txt cleaned_ai_review/cleaned_review10-ai.txt
 
 #7. concat data to specific we want.5K human and 5K AI
 
 #concat human data 5K
-    # python3 utility/concat_data.py cleaned_review/cleaned_review1.txt cleaned_review/cleaned_review2.txt cleaned_review/cleaned_review3.txt cleaned_review/cleaned_review5.txt cleaned_review/cleaned_review8.txt concated_review_5k.txt
+    python3 utility/concat_data.py cleaned_review/cleaned_review1.txt cleaned_review/cleaned_review2.txt cleaned_review/cleaned_review3.txt cleaned_review/cleaned_review5.txt cleaned_review/cleaned_review8.txt concated_review_5k.txt
 #concat ai data 5K
-    # python3 utility/concat_data.py cleaned_ai_review/cleaned_review1-ai.txt cleaned_ai_review/cleaned_review2-ai.txt cleaned_ai_review/cleaned_review3-ai.txt cleaned_ai_review/cleaned_review5-ai.txt cleaned_ai_review/cleaned_review8-ai.txt concated_ai_review_5k.txt
+    python3 utility/concat_data.py cleaned_ai_review/cleaned_review1-ai.txt cleaned_ai_review/cleaned_review2-ai.txt cleaned_ai_review/cleaned_review3-ai.txt cleaned_ai_review/cleaned_review5-ai.txt cleaned_ai_review/cleaned_review8-ai.txt concated_ai_review_5k.txt
 
 #8 make both data into one csv file and alternating between human and ai review
 #since after concat we have a number in front of each line
 #we need to remove before make csv
-    # python3 utility/rm_line_number.py concated_review_5k.txt
-    # python3 utility/rm_line_number.py concated_ai_review_5k.txt
+    python3 utility/rm_line_number.py concated_review_5k.txt
+    python3 utility/rm_line_number.py concated_ai_review_5k.txt
 
 #then make csv file and alternating between human and ai review
-    # python3 make_csv.py concated_review_5k.txt concated_ai_review_5k.txt review-human-ai.csv
+    python3 make_csv.py concated_review_5k.txt concated_ai_review_5k.txt review-human-ai.csv
 #9 minddy take the process of model training
 
 #done
